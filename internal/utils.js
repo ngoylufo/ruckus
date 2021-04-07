4a916b9fabafa2ecd197cfe9f2154c94a0b7bf3c
Ngoy Lufo <ngoylufo@gmail.com>
2021-04-07 12:23:04 +0200

    Added some more utility functions

--- internal/utils.js
+++ internal/utils.js
@@ -1,3 +1,9 @@
+export const compose = (...fns) => {
+  return (x) => fns.reduceRight((arg, fn) => fn(arg), x);
+};
+
+export const partial = (fn, ...args) => fn.bind(null, ...args);
+
 export const memoize = (callback, serialize = JSON.stringify) => {
   const cache = {};
 
