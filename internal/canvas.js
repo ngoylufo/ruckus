fa96cade15392cbe5cf159e32c205f2021d57e11
Ngoy Lufo <ngoylufo@gmail.com>
2021-04-07 12:41:35 +0200

    Fixed issue with canvas resize and mouse tracking functions.

--- internal/canvas.js
+++ internal/canvas.js
@@ -13,24 +13,24 @@ const createResizer = memoize(({ dimensions, trackMouse }) => {
     return () => {
       const ctx = globals.get("ruckus.canvas.ctx");
       const rect = ctx.canvas.getBoundingClientRect();
-      globals.merge("ruckus.canvas.rect", rect);
+
+      ctx.canvas.width = window.innerWidth;
+      ctx.canvas.height = window.innerHeight;
+      globals.set("ruckus.canvas.rect", rect);
     };
   }
 
   return () => {
     const ctx = globals.get("ruckus.canvas.ctx");
     const rect = ctx.canvas.getBoundingClientRect();
-
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    globals.merge("ruckus.canvas.rect", rect);
+    globals.set("ruckus.canvas.rect", rect);
   };
 });
 
 export const write = (ctx, text, { x, y, alpha, font, style }) => {
   ctx.globalAlpha = alpha ?? 1;
-  ctx.font = font ?? '24px Arial';
-  ctx.fillStyle = style ?? 'black';
+  ctx.font = font ?? "24px Arial";
+  ctx.fillStyle = style ?? "black";
   ctx.fillText(text, x ?? 0, y ?? 0);
 };
 
@@ -44,14 +44,15 @@ export const clear = (ctx) => {
 };
 
 export const init = ({ element, dimensions, trackMouse }) => {
-  const ctx = globals.set("ruckus.canvas.ctx", getContext(element));
   const resize = createResizer({ dimensions, trackMouse });
+  globals.set("ruckus.canvas.ctx", getContext(element));
 
   if (trackMouse) {
     const mouse = { x: 0, y: 0, width: 0.1, height: 0.1 };
-    const rect = globals.merge("ruckus.canvas.rect", {});
 
     element.addEventListener("mousemove", (ev) => {
+      const rect = globals.get("ruckus.canvas.rect");
+
       mouse.x = ev.x - rect.left;
       mouse.y = ev.y - rect.top;
     });
